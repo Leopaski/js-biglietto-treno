ab2f449ec367fed767dbe4dd6cf10b26391e7989
Leopaski <leonardo.pascale@gmail.com>
2023-05-12 20:09:43 +0200

    toFixed

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,11 @@
-let tripdistance = parceInt (prompt('da quanti km è composto il tuo viaggio'?));
-const passengerage = parceInt (prompt ('Inserisci la tua età'));
-let priceticket = tripdistance*0,21 + 'euro';
-let scontojunior = 20*priceticket/100;
-let scontosenior = 40*priceticket/100;
+let tripdistance = parseInt(prompt('da quanti km è composto il tuo viaggio'))
+const passengerage = parseInt(prompt('Inserisci la tua età'))
+let priceticket = tripdistance*0.21 
+let scontojunior = 20*priceticket/100
+let scontosenior = 40*priceticket/100
 const ticket = priceticket;
-const ticketjunior = priceticket - scontojunior + 'euro';
-const ticketsenior = priceticket - scontosenior +'euro';
+const ticketjunior = (priceticket - scontojunior).toFixed(2);
+const ticketsenior = (priceticket - scontosenior).toFixed(2);
 if (passengerage<18){
     document.getElementById("costoticket").innerHTML += ticketjunior + 'euro';
 } else if (passengerage>65) {
